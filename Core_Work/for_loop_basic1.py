321f791c58608f2fa08b4bb038e1c0da085c359f
Sloan Molstad <sloan.molstad@gmail.com>
2023-03-09 18:28:12 -0800

    Here is my core work folder

---
+++ Core_Work/for_loop_basic1.py
@@ -0,0 +1,13 @@
+# Basic - Print all integers from 0 to 150.
+
+# for x in range(151):
+#     print(x)
+
+# Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+# for y in range(5, 1001, 5):
+#     print(y)
+
+# Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print 
+# "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+for (int a = 1; a <= 100; a )
